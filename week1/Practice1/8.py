2c06f9cf8bde36bb586db1e84abe33ba188e8458
Adilet Mirlanuly <a_mirlanuly@kbtu.kz>
2026-01-20 12:29:34 +0500

    Data Types

---
+++ week1/Practice1/8.py
@@ -0,0 +1,22 @@
+x = "Hello World"
+
+x = 20.5 #float
+
+x = ["apple", "banana", "cherry"] #list 
+
+x = {"name" : "John", "age" : 36} #dict
+
+x = True #bool
+
+
+#SPECIFIC DATA TYPES
+
+x = str("Hello World") 
+
+x = float(20.5)
+
+x = list(("apple", "banana", "cherry"))
+
+x = dict(name="John", age=36)
+
+x = bytearray(5)
