418abd1d43ce13fcaee18b04a72e766e89332e18
Adilet Mirlanuly <a_mirlanuly@kbtu.kz>
2026-01-20 12:34:29 +0500

    String

---
+++ week1/Practice1/11.py
@@ -0,0 +1,35 @@
+print("Hello")
+print('Hello')
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+
