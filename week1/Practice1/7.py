706d85d9f8913377b986892095180926cd55364e
Adilet Mirlanuly <a_mirlanuly@kbtu.kz>
2026-01-20 12:26:49 +0500

    Variable

---
+++ week1/Practice1/7.py
@@ -0,0 +1,63 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+a = 4       # a is of type int
+a = "Sally" # a is now of type str
+print(a)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+#2myvar = "John"
+#my-var = "John"
+#my var = "John" #error 
+
+myVariableName = "John"
+
+my_variable_name = "John"
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
