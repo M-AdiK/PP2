afbfd4b5f63b484242fd8a51886336b7dfd4643e
Mirlanuly Adilet <uwutop63@gmail.com>
2026-01-31 12:55:25 +0500

    Sets end

---
+++ w3school/Practice2/5.1.py
@@ -0,0 +1,36 @@
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1 | set2
+print(set3)
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1.union(set2, set3, set4)
+print(myset)
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1 | set2
+print(set3)
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+
