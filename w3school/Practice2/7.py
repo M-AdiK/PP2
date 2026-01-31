e7c4545fcbb8ad2bf71716e36e75b4d5576bbd93
Mirlanuly Adilet <uwutop63@gmail.com>
2026-01-31 13:01:00 +0500

    If elif else

---
+++ w3school/Practice2/7.py
@@ -0,0 +1,138 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+number = 15
+if number > 0:
+  print("The number is positive")
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+age = 25
+
+if age < 13:
+  print("You are a child")
+elif age < 20:
+  print("You are a teenager")
+elif age < 65:
+  print("You are an adult")
+elif age >= 65:
+  print("You are a senior")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
+
+score = 85
+attendance = 90
+submitted = True
+
+if score >= 60:
+  if attendance >= 80:
+    if submitted:
+      print("Pass with good standing")
+    else:
+      print("Pass but missing assignment")
+  else:
+    print("Pass but low attendance")
+else:
+  print("Fail")
+
+a = 33
+b = 200
+
+if b > a:
+  pass
+
