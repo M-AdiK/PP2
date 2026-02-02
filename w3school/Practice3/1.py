50392fc7d4dea9b483ec3cde2a36fe211a00539e
Mirlanuly Adilet <uwutop63@gmail.com>
2026-02-02 11:37:40 +0500

    Functions

---
+++ w3school/Practice3/1.py
@@ -0,0 +1,18 @@
+def my_function():
+  print("Hello from a function")
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+calculate_sum()
+_private_function()
+myFunction2()
