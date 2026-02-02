dc78795391c061731ae21712b00d410029bc76b6
Mirlanuly Adilet <uwutop63@gmail.com>
2026-02-02 11:44:06 +0500

    Arguments

---
+++ w3school/Practice3/2.py
@@ -0,0 +1,22 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def my_function(name): # name is a parameter
+  print("Hello", name)
+
+my_function("Emil") # "Emil" is an argument
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil") #error потому что функция ожидает 2 параметра а там 1
+
