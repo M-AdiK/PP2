a17a4632e1be7120aab31a15227f833110be9cc9
Mirlanuly Adilet <uwutop63@gmail.com>
2026-02-02 11:38:35 +0500

    Functions

--- w3school/Practice3/1.py
+++ w3school/Practice3/1.py
@@ -16,3 +16,33 @@ my_function()
 calculate_sum()
 _private_function()
 myFunction2()
+
+def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message)
+
+
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
+
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
+
+temp2 = 95
+celsius2 = (temp2 - 32) * 5 / 9
+print(celsius2)
+
+temp3 = 50
+celsius3 = (temp3 - 32) * 5 / 9
+print(celsius3)
+
+
+def my_function():
+  pass
