296eed53560e3ff43ffb15a08b1ae262fe37aa59
Mirlanuly Adilet <uwutop63@gmail.com>
2026-02-03 11:03:29 +0500

    Class 2 part

---
+++ w3school/Practice3/8.1.py
@@ -0,0 +1,187 @@
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil")
+p1.greet()
+
+
+class Calculator:
+  def add(self, a, b):
+    return a + b
+
+  def multiply(self, a, b):
+    return a * b
+
+calc = Calculator()
+print(calc.add(5, 3))
+print(calc.multiply(4, 7))
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def get_info(self):
+    return f"{self.name} is {self.age} years old"
+
+p1 = Person("Tobias", 28)
+print(p1.get_info())
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def celebrate_birthday(self):
+    self.age += 1
+    print(f"Happy birthday! You are now {self.age}")
+
+p1 = Person("Linus", 25)
+p1.celebrate_birthday()
+p1.celebrate_birthday()
+
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+
+
+class Student(Person):
+  pass
+
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    #add properties etc.
+
+
+x = "Hello World!"
+
+print(len(x))
+
+
+mytuple = ("apple", "banana", "cherry")
+
+print(len(mytuple))
+
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(len(thisdict))
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age # Private property
+
+p1 = Person("Emil", 25)
+print(p1.name)
+print(p1.__age) # This will cause an error\
+
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+  def set_age(self, age):
+    if age > 0:
+      self.__age = age
+    else:
+      print("Age must be positive")
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+p1.set_age(26)
+print(p1.get_age())
+
+
+class Outer:
+  def __init__(self):
+    self.name = "Outer Class"
+
+  class Inner:
+    def __init__(self):
+      self.name = "Inner Class"
+
+    def display(self):
+      print("This is the inner class")
+
+outer = Outer()
+print(outer.name)
+
+
+class Outer:
+  def __init__(self):
+    self.name = "Outer"
+
+  class Inner:
+    def __init__(self):
+      self.name = "Inner"
+
+    def display(self):
+      print("Hello from inner class")
+
+outer = Outer()
+inner = outer.Inner()
+inner.display()
+
+
+class Outer:
+  def __init__(self):
+    self.name = "Emil"
+
+  class Inner:
+    def __init__(self, outer):
+      self.outer = outer
+
+    def display(self):
+      print(f"Outer class name: {self.outer.name}")
+
+outer = Outer()
+inner = outer.Inner(outer)
+inner.display()
+
