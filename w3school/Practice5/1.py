2310b886127302d80651b88504155b09a1584dd8
Mirlanuly Adilet <uwutop63@gmail.com>
2026-02-23 12:26:39 +0500

    Re first

--- w3school/Practice5/1.py
+++ w3school/Practice5/1.py
@@ -1,7 +1,100 @@
 import re
 
-txt = input()
-x = re.search("^Hello" , txt)
-if x :
-    print("Yes")
-else : print("No")
+#Check if the string starts with "The" and ends with "Spain":
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if x:
+  print("YES! We have a match!")
+else:
+  print("No match")
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("\s", txt)
+
+print("The first white-space character is located in position:", x.start())
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("Portugal", txt)
+print(x)
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt)
+print(x)
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 2)
+print(x)
+
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.string)
+
+
+
+
+import re
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
+
+
