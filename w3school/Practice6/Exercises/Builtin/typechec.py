3896aa60839ce764ef21f9304b3815a56a5b197e
Mirlanuly Adilet <uwutop63@gmail.com>
2026-03-03 12:26:52 +0500

    Add Practice6 - file handling and built-in functions examples

---
+++ w3school/Practice6/Exercises/Builtin/typechec.py
@@ -0,0 +1,11 @@
+x = 10
+
+print(type(x))            # shows type
+print(isinstance(x, int)) # True/False
+
+int("5")        # string → int
+float("3.14")   # string → float
+str(100)        # number → string
+list("abc")     # → ['a','b','c']
+tuple([1,2])
+set([1,1,2,3])
