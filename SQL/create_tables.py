e19d0104d74cd434298f9b9d4f7cd313a74b947d
Mirlanuly Adilet <uwutop63@gmail.com>
2026-03-29 18:26:19 +0500

    Tables

--- SQL/create_tables.py
+++ SQL/create_tables.py
@@ -3,7 +3,7 @@ from config import load_config
 
 def create_tables():
     """ Создаем таблицу contacts в базе данных """
-    # SQL-запрос для создания таблицы
+
     command = """
     CREATE TABLE IF NOT EXISTS contacts (
         user_id SERIAL PRIMARY KEY,
@@ -15,9 +15,9 @@ def create_tables():
     try:
         with psycopg2.connect(**config) as conn:
             with conn.cursor() as cur:
-                # Выполняем SQL-команду
+
                 cur.execute(command)
-            # Фиксируем изменения (commit происходит автоматически при выходе из with conn)
+
             print("✅ Таблица 'contacts' успешно создана или уже существует.")
     except (psycopg2.DatabaseError, Exception) as error:
         print(f"❌ Ошибка при создании таблицы: {error}")
